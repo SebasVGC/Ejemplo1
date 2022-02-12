3696062bb543081212b4633c7d5cf11f41f86526
SebasVGC <2902340180301@ingenieria.usac.edu.gt>
2022-02-11 19:33:09 -0600

    Final ejemplo

--- Clase 4/Clase 4/Form1.vb
+++ Clase 4/Clase 4/Form1.vb
@@ -51,4 +51,92 @@
         Next
         MsgBox("Cantidad A: " + cantidadTotalA.ToString)
     End Sub
+
+    Private Sub btnCalcular_Click(sender As Object, e As EventArgs) Handles btnCalcular.Click
+        Dim moneda As String
+        Dim cantidadOp As Integer
+        Dim texto As String
+        Dim total1 As Double = 0
+        Dim total2 As Double = 0
+
+        If (RadioButton1.Checked) Then
+            moneda = "dolar"
+        ElseIf (RadioButton2.Checked) Then
+            moneda = "peso"
+        ElseIf (RadioButton3.Checked) Then
+            moneda = "euro"
+        End If
+
+        texto = txtCantidad.Text
+        If (texto = "") Then
+            MsgBox("no se adminten textos vacios")
+        Else
+            cantidadOp = Int32.Parse(texto)
+            If (cantidadOp > 0) Then
+                If (moneda = "dolar") Then
+                    total1 = 7.69 * cantidadOp
+                ElseIf (moneda = "peso") Then
+                    total1 = 0.38 * cantidadOp
+                ElseIf (moneda = "euro") Then
+                    total1 = 8.79 * cantidadOp
+                End If
+            Else
+                MsgBox("Ingrese numero valido")
+            End If
+
+            MsgBox(total1)
+
+        End If
+
+
+
+
+    End Sub
+
+    Private Sub Button9_Click(sender As Object, e As EventArgs) Handles Button9.Click
+        Dim moneda As String
+        Dim cantidadOp As Integer
+        Dim texto As String
+        Dim total1 As Double = 0
+        Dim total2 As Double = 0
+
+        If (RadioButton1.Checked) Then
+            moneda = "dolar"
+        ElseIf (RadioButton2.Checked) Then
+            moneda = "peso"
+        ElseIf (RadioButton3.Checked) Then
+            moneda = "euro"
+        End If
+
+        texto = TextBox2.Text
+        If (texto = "") Then
+            MsgBox("no se adminten textos vacios")
+        Else
+            cantidadOp = Int32.Parse(texto)
+            For index As Integer = 1 To cantidadOp
+                Dim dinero As String
+                dinero = InputBox("Ingrese monto", "Saludo", "", 2)
+                Dim dineroInt = Int32.Parse(dinero)
+                If (moneda = "dolar") Then
+                    total1 = 7.69 * dineroInt
+                    MsgBox(total1)
+                    total2 = total2 + total1
+
+                ElseIf (moneda = "peso") Then
+                    total1 = 0.38 * dineroInt
+                    MsgBox(total1)
+                    total2 = total2 + total1
+
+                ElseIf (moneda = "euro") Then
+                    total1 = 8.79 * dineroInt
+                    MsgBox(total1)
+                    total2 = total2 + total1
+
+                End If
+            Next
+        End If
+
+        Label3.Text = total2.ToString
+
+    End Sub
 End Class
